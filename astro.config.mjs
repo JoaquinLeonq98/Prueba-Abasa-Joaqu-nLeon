09efb1f36f15cb474ae2578da2e718fac65be3b5
Joaquin <joaquin.leon@xidden.tech>
2025-11-13 00:58:35 -0600

    se añaden dependencias para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
-import vercel from '@astrojs/vercel/serverless'; 
+import vercel from '@astrojs/vercel/serverless';
 import tailwindcss from '@tailwindcss/vite';
 
 
-// https://astro.build/config
 export default defineConfig({
-
+    integrations: [
+        react(), 
+    ],
+    
+	vite: {    plugins: [tailwindcss()],  },
     output: 'server', 
-    adapter: vercel({}),
-    vite: {
-        plugins: [tailwindcss()],
-    },
+    adapter: vercel({}), 
+    
+
 });
